f2bdb52ab890a20a63a88ec37dfbea606d62e9d3
l.rsouza@outlook.com <lucasfr@programmer.net>
2018-11-02 15:53:43 -0200

    added new controller, added swagger integration, added CORS to test consume.

--- Chama.Courses.API/Startup.cs
+++ Chama.Courses.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Chama.Courses.API
 {
@@ -17,6 +18,10 @@ namespace Chama.Courses.API
     {
         public Startup(IConfiguration configuration)
         {
+
+            
+
+
             Configuration = configuration;
         }
 
@@ -25,12 +30,33 @@ namespace Chama.Courses.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigins",
+                builder =>
+                {
+                    builder.AllowAnyHeader();
+                    builder.AllowAnyMethod();
+                    builder.AllowAnyOrigin();
+                });
+            });
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Courses", Version = "v1" });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -40,8 +66,21 @@ namespace Chama.Courses.API
                 app.UseHsts();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Courses V1");
+            });
+
+
+            app.UseCors("AllowSpecificOrigins");
             app.UseHttpsRedirection();
             app.UseMvc();
+
+
+            
+
+
         }
     }
 }
